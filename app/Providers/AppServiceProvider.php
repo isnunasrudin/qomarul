7c3c5d30bf60bfbbd348d01defbd767cc407835f
Isnu Nasrudin <isnunas@gmail.com>
2026-08-12 18:39:39 +0700

    feat: force https scheme in production environment within AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Employee;
 use App\Observers\EmployeeObserver;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (!App::environment('local')) {
+            URL::forceScheme('https');
+        }
         Employee::observe(EmployeeObserver::class);
     }
 }
